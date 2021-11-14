e355b55f26a74bdfb49228df313bd87591e02f2c
Krishan Mistry <kvjmistry@gmail.com>
2021-11-14 10:07:32 -0600

    updating merge script to include other branch

--- scripts/merge_files.py
+++ scripts/merge_files.py
@@ -9,17 +9,21 @@ print(files)
 
 Metadata = []
 Energy   = []
+Other    = []
 
 for f in files:
     Metadata.append(pd.read_hdf(f,'Metadata'))
     Energy.append(pd.read_hdf(f,'Energy'))
+    Other.append(pd.read_hdf(f,'Other'))
 
 Metadata_m = pd.concat(Metadata)
 Energy_m   = pd.concat(Energy)
+Other_m    = pd.concat(Other)
 
 print(Metadata_m)
 
 file_out = sys.argv[2]
 Energy_m.to_hdf(file_out,'Energy')
 Metadata_m.to_hdf(file_out,'Metadata')
+Other_m.to_hdf(file_out,'Other')
 
